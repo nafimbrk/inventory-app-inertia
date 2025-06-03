6407aad370945769bf261378fcad953afc3d95d9
nafimbrk <nafimubarok2005@gmail.com>
2025-06-03 03:08:13 -0700

    add layout

---
+++ resources/js/Pages/Items/Index.vue
@@ -0,0 +1,50 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    items: Array
+})
+</script>
+<template>
+    <Head title="Items" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Item List
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900">
+                        <table class="table-auto w-full mt-4">
+            <thead>
+                <tr>
+                    <th class="px-4 py-2">#</th>
+                    <th class="px-4 py-2">Name</th>
+                    <th class="px-4 py-2">Qty</th>
+                    <th class="px-4 py-2">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in items" :key="index">
+                    <td class="border px-4 py-2">{{ ++index }}</td>
+                    <td class="border px-4 py-2">{{ item.name }}</td>
+                    <td class="border px-4 py-2">{{ item.qty }}</td>
+                    <td class="border px-4 py-2">
+                        edit | stock card
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
